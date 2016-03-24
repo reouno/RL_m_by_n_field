fc102818543689bbb055a7b953d07d6b5b5fa6de
reouno <reo@MacBook-Air.local>
2016-03-24 23:39:05 +0900

    cleaned up the code

--- app/Main.hs
+++ app/Main.hs
@@ -70,53 +70,6 @@ learned_actions field current_position qs trace = do
             return (trace' ++ [new_position])
         else learned_actions field new_position qs trace'
 
-episode_io :: Int
-              -> Vec.Vector (Vec.Vector Double)
-              -> (Int, Int)
-              -> Lib.Cross
-              -> Map.Map (Int, Int) (Map.Map Lib.Cross Double)
-              -> [Double]
-              -> IO (Int, Map.Map (Int, Int) (Map.Map Lib.Cross Double))
-episode_io t field prev_position action qs randNums = do
-    let current_position = move prev_position action
-    let reward = get_reward field current_position
-    let (_, qs') = get_Qs field current_position qs $ round $ (100 * randNums!!0)
-    let qs'' = update_Q prev_position action reward qs' $ randNums!!1
-    let possible_actions = Map.keys (qs'' Map.! current_position)
-    let action_qs = Map.elems $ qs'' Map.! current_position
-    let probs = calc_p action_qs $ temper (t :: Int)
-    let action_index = take_action (t :: Int) (Map.elems $ qs'' Map.! current_position) $ randNums!!2
-    let action' = Map.keys (qs'' Map.! current_position) !! (take_action (t :: Int) (Map.elems $ qs'' Map.! current_position) $ randNums!!2)
-    if t > 93
-        then do
-            print $ "current_position = " ++ show current_position
-            print $ "reward = " ++ show reward
-            print $ "possible_actions = " ++ show possible_actions
-            print $ "action_qs = " ++ show action_qs
-            print $ "probs = " ++ show probs
-            print $ "action_index = " ++ show action_index 
-            print $ "action' = " ++ show action'
-        else return ()
-    print current_position
-    if current_position == (length field - 1, length (field Vec.! 0) - 1) -- coordinates of the Goal
-        then return (t+1, qs'')
-        else episode_io (t+1) field current_position action' qs'' $ drop 3 randNums
-
-step_by_step :: Int
-                -> Vec.Vector (Vec.Vector Double)
-                -> Map.Map (Int, Int) (Map.Map Lib.Cross Double)
-                -> IO ()
-step_by_step t field qs = do
-    rand <- getStdRandom random :: IO Int
-    let randNums = randomRs (0, 0.9999) $ mkStdGen rand :: [Double]
-    let action = Map.keys (qs Map.! (0,0)) !! (take_action t (Map.elems $ qs Map.! (0,0)) $ randNums!!0)
-    (times_of_actions, qs') <- episode_io t field (0, 0) action qs $ drop 1 randNums
-    print $ times_of_actions - t
-    print $ "t = " ++ show t
-    if t > 100
-        then return ()
-        else step_by_step (t+1) field qs'
-
 learn_1000 :: IO Int
 learn_1000 = do
     rand <- getStdRandom random :: IO Int
@@ -143,3 +96,51 @@ success_rate n = do
         loop _ sum_i = return $ sum_i / n
     loop 0 0
 -- n回episodesを実行して、学習成功率を返す。
+
+-- 以下の2つの処理は、デバッグ用に作ったものだから、main実行には必要ない
+--episode_io :: Int
+--              -> Vec.Vector (Vec.Vector Double)
+--              -> (Int, Int)
+--              -> Lib.Cross
+--              -> Map.Map (Int, Int) (Map.Map Lib.Cross Double)
+--              -> [Double]
+--              -> IO (Int, Map.Map (Int, Int) (Map.Map Lib.Cross Double))
+--episode_io t field prev_position action qs randNums = do
+--    let current_position = move prev_position action
+--    let reward = get_reward field current_position
+--    let (_, qs') = get_Qs field current_position qs $ round $ (100 * randNums!!0)
+--    let qs'' = update_Q prev_position action reward qs' $ randNums!!1
+--    let possible_actions = Map.keys (qs'' Map.! current_position)
+--    let action_qs = Map.elems $ qs'' Map.! current_position
+--    let probs = calc_p action_qs $ temper (t :: Int)
+--    let action_index = take_action (t :: Int) (Map.elems $ qs'' Map.! current_position) $ randNums!!2
+--    let action' = Map.keys (qs'' Map.! current_position) !! (take_action (t :: Int) (Map.elems $ qs'' Map.! current_position) $ randNums!!2)
+--    if t > 93
+--        then do
+--            print $ "current_position = " ++ show current_position
+--            print $ "reward = " ++ show reward
+--            print $ "possible_actions = " ++ show possible_actions
+--            print $ "action_qs = " ++ show action_qs
+--            print $ "probs = " ++ show probs
+--            print $ "action_index = " ++ show action_index 
+--            print $ "action' = " ++ show action'
+--        else return ()
+--    print current_position
+--    if current_position == (length field - 1, length (field Vec.! 0) - 1) -- coordinates of the Goal
+--        then return (t+1, qs'')
+--        else episode_io (t+1) field current_position action' qs'' $ drop 3 randNums
+--
+--step_by_step :: Int
+--                -> Vec.Vector (Vec.Vector Double)
+--                -> Map.Map (Int, Int) (Map.Map Lib.Cross Double)
+--                -> IO ()
+--step_by_step t field qs = do
+--    rand <- getStdRandom random :: IO Int
+--    let randNums = randomRs (0, 0.9999) $ mkStdGen rand :: [Double]
+--    let action = Map.keys (qs Map.! (0,0)) !! (take_action t (Map.elems $ qs Map.! (0,0)) $ randNums!!0)
+--    (times_of_actions, qs') <- episode_io t field (0, 0) action qs $ drop 1 randNums
+--    print $ times_of_actions - t
+--    print $ "t = " ++ show t
+--    if t > 100
+--        then return ()
+--        else step_by_step (t+1) field qs'
